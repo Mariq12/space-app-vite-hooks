e401dc57e4d7d70932798012e20e61a3819a0811
Mariq12 <fmlabanda@utpl.edu.ec>
2024-06-12 08:38:30 -0500

    Se formateo al código

--- src/hooks/usePhotoModal.jsx
+++ src/hooks/usePhotoModal.jsx
@@ -3,19 +3,19 @@ import { GlobalContext } from "../context/GlobalContext";
 
 
 function usePhotoModal() {
-  const { state, dispatch } = useContext(GlobalContext);
+    const { state, dispatch } = useContext(GlobalContext);
 
-  const openPhotoModal = (photo) => {
-    dispatch({ type: 'SET_SELECTED_PHOTO', payload: photo })
-  };
+    const openPhotoModal = (photo) => {
+        dispatch({ type: 'SET_SELECTED_PHOTO', payload: photo })
+    };
 
-  const closePhotoModal = () => {
-    dispatch ({ type: "SET_SELECTED_PHOTO", payload: null})
-  };
+    const closePhotoModal = () => {
+        dispatch({ type: "SET_SELECTED_PHOTO", payload: null })
+    };
 
-  const selectedPhoto = state.selectedPhoto;
-  const isOpenModal = state.modalOpen;
-  return {selectedPhoto, isOpenModal, openPhotoModal, closePhotoModal};
+    const selectedPhoto = state.selectedPhoto;
+    const isOpenModal = state.modalOpen;
+    return { selectedPhoto, isOpenModal, openPhotoModal, closePhotoModal };
 }
 
 export default usePhotoModal;
